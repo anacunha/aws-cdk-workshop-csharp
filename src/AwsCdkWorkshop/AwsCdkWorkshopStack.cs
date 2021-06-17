e0d42b3638f40bd1fdf4b64b5476c22b4d68c41f
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2021-06-17 14:02:43 -0300

    Add Lambda function

--- src/AwsCdkWorkshop/AwsCdkWorkshopStack.cs
+++ src/AwsCdkWorkshop/AwsCdkWorkshopStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
 
 namespace AwsCdkWorkshop
 {
@@ -6,6 +7,12 @@ namespace AwsCdkWorkshop
     {
         internal AwsCdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
+            var hello = new Function(this, "HelloHandler", new FunctionProps
+            {
+                Runtime = Runtime.NODEJS_14_X,
+                Code = Code.FromAsset("lambda"),
+                Handler = "hello.handler"
+            });
         }
     }
 }
